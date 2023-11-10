80cc173e1bb47a4adadd3620372db435ed61ddf7
Xianhan Qin <q764579066@gmail.com>
2023-11-10 02:26:36 +0000

    add function.py

---
+++ src/function.py
@@ -0,0 +1,60 @@
+import numpy as np
+import pandas as pd
+
+
+def difference(x, y):
+    return np.abs(x - y)
+
+
+def init_energy(labels_mtx, pixels, beta, cls_para, neighbor_indices, in_tissue):
+    labels_mtx
+    energy = 0.0
+    rows, cols = labels_mtx.shape
+    print(rows, cols)
+    for i in range(rows):
+        for j in range(cols):
+            mean, var = cls_para[labels_mtx[i, j]]
+            energy += np.log(np.sqrt(2 * np.pi * var))
+            energy += (pixels[i, j] - mean) ** 2 / (2 * var)
+            for a, b in neighbor_indices:
+                a += i
+                b += j
+                # print(a, b)
+                if (a < rows) and (b < cols) and in_tissue[a, b]:
+                    energy += beta * difference(labels_mtx[i, j], labels_mtx[a, b])
+    return energy
+
+
+def delta_energy(labels_mtx, pixels, index, new_label, beta, cls_para, neighbor_indices, in_tissue):
+    labels_mtx
+    (i, j) = index
+    rows, cols = labels_mtx.shape
+    mean, var = cls_para[labels_mtx[i, j]]
+    init_energy = np.log(np.sqrt(2 * np.pi * var)) + (pixels[i, j] - mean) ** 2 / (2 * var)
+    for a, b in neighbor_indices:
+        a += i
+        b += j
+        if (a < rows) and (b < cols) and in_tissue[a, b]:
+            init_energy += beta * difference(labels_mtx[i, j], labels_mtx[a, b])
+    mean_new, var_new = cls_para[new_label]
+    new_energy = np.log(np.sqrt(2 * np.pi * var_new)) + (pixels[i, j] - mean_new) ** 2 / (2 * var_new)
+    for a, b in neighbor_indices:
+        a += i
+        b += j
+        if (a < rows) and (b < cols) and in_tissue[a, b]:
+            new_energy += beta * difference(new_label, labels_mtx[a, b])
+    # print (new_energy, init_energy)
+    return new_energy - init_energy
+
+
+def temp_function(method="exponential"):
+    if method == "exponential":
+        return lambda x: 0.99 * x
+
+
+def distribution_update(new_label, cluster, cls_para):
+    mean = np.mean(cluster)
+    var = np.var(cluster)
+    if var == 0:
+        var = 1e-5
+    cls_para[new_label] = (mean, var)
--- src/mrf.py
+++ src/mrf.py
@@ -6,64 +6,7 @@ import multiprocessing as mp
 import random
 from sklearn import mixture
 from tqdm import tqdm
-
-
-def difference(x, y):
-    return np.abs(x - y)
-
-
-def init_energy(labels_mtx, pixels, beta, cls_para, neighbor_indices, in_tissue):
-    labels_mtx
-    energy = 0.0
-    rows, cols = labels_mtx.shape
-    print(rows, cols)
-    for i in range(rows):
-        for j in range(cols):
-            mean, var = cls_para[labels_mtx[i, j]]
-            energy += np.log(np.sqrt(2 * np.pi * var))
-            energy += (pixels[i, j] - mean) ** 2 / (2 * var)
-            for a, b in neighbor_indices:
-                a += i
-                b += j
-                # print(a, b)
-                if (a < rows) and (b < cols) and in_tissue[a, b]:
-                    energy += beta * difference(labels_mtx[i, j], labels_mtx[a, b])
-    return energy
-
-
-def delta_energy(labels_mtx, pixels, index, new_label, beta, cls_para, neighbor_indices, in_tissue):
-    labels_mtx
-    (i, j) = index
-    rows, cols = labels_mtx.shape
-    mean, var = cls_para[labels_mtx[i, j]]
-    init_energy = np.log(np.sqrt(2 * np.pi * var)) + (pixels[i, j] - mean) ** 2 / (2 * var)
-    for a, b in neighbor_indices:
-        a += i
-        b += j
-        if (a < rows) and (b < cols) and in_tissue[a, b]:
-            init_energy += beta * difference(labels_mtx[i, j], labels_mtx[a, b])
-    mean_new, var_new = cls_para[new_label]
-    new_energy = np.log(np.sqrt(2 * np.pi * var_new)) + (pixels[i, j] - mean_new) ** 2 / (2 * var_new)
-    for a, b in neighbor_indices:
-        a += i
-        b += j
-        if (a < rows) and (b < cols) and in_tissue[a, b]:
-            new_energy += beta * difference(new_label, labels_mtx[a, b])
-    # print (new_energy, init_energy)
-    return new_energy - init_energy
-
-
-def temp_function(method="exponential"):
-    if method == "exponential":
-        return lambda x: 0.99 * x
-
-
-def distribution_update(new_label, cluster, cls_para):
-    mean = np.mean(cluster)
-    var = np.var(cluster)
-    if var == 0:
-        var = 1e-5
-    cls_para[new_label] = (mean, var)
+from function import *
 
 
 def annealing(
@@ -142,8 +85,8 @@ def icm_em_process(
     in_tissue,
     coord,
     exp,
-    icm_iter=10,
-    max_iter=10,
+    icm_iter=3,
+    max_iter=5,
 ):
     for _ in tqdm(range(max_iter)):
         # ICM step
@@ -171,6 +114,7 @@ def icm_em_process(
                 )
                 # print(temp_delta)
                 if temp_delta < 0:
+                    
                     labels_mtx[i, j] = new_label
                     delta += temp_delta
                     changed += 1
