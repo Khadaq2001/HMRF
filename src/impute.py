d633d9ffbcbd4d1fbe95323ad5df2e1dc1e6e45e
Xianhan Qin <q764579066@gmail.com>
2023-11-16 02:46:00 +0000

    test function

--- src/impute.py
+++ src/impute.py
@@ -2,18 +2,33 @@ import numpy as np
 import pandas as pd
 import scanpy as sc
 
+
 def impute(
     exp: np.ndarray,
-    label_list : np.ndarray,
+    label_list: np.ndarray,
     threshld: float,
+    shift: float,
 ):
     """
     Impute single gene expression spatial transcriptomics data from same label cells.
     """
     for label in label_list:
         idx = np.where(label_list == label)
-        cluster_exp = exp[:,idx ]
-        
+        cluster_exp = exp[:, idx]
+        min_index = np.argmin(cluster_exp)
+        cluster_exp[min_index] = np.mean(cluster_exp)
+        exp[:, idx] = cluster_exp + shift
+    return exp
+
+
 def main():
-       
-    ...
+    test_data = pd.read_csv("./temp/test_data.csv")
+    test_data = test_data.values
+    test_label = pd.read_csv("./test_label.csv")
+    print(test_data)
+    imputed_data = impute(test_data, test_label, 0.1, 0.1)
+    print(imputed_data)
+
+
+if __name__ == "__main__":
+    main()
